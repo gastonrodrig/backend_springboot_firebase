ceb566c3b96942f5d3badb61c51eec6a3c53468c
Gaston <gaston.rodriguez@icloud.com>
2024-12-20 13:47:15 -0500

    startUp swagger message added

--- src/main/java/com/gato/multi/MultiApplication.java
+++ src/main/java/com/gato/multi/MultiApplication.java
@@ -8,6 +8,10 @@ public class MultiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultiApplication.class, args);
+		System.out.println("\n========================================");
+		System.out.println("Aplicación iniciada. Accede a Swagger UI en:");
+		System.out.println("http://localhost:8080/swagger-ui/index.html#");
+		System.out.println("========================================");
 	}
 
 }
